8d630ca1528f248c0d2f914c9966211ed7f11612
3Nd_R1m <ben.bergeron1@gmail.com>
2019-02-28 20:27:17 -0500

    Refactoring - voitures.php
    Nouvelle structure:
    $tab_voiture = array(
    "marque" => array(
    "model" => array(
    "description => "",
    "price"=> array()
    )
    )
     ...
    )

--- modeles/voitures.php
+++ modeles/voitures.php
@@ -1,77 +1,135 @@
 <?php
-$tab_voiture = Array("Kia" => Array(
-                                    "Forte" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",8995),
-                                    "Forte" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",9000),
-                                    "Forte" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",11000),
-                                    "Optima" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",10995),
-                                    "Optima" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",10995),
-                                    "Optima" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",12000),
-                                    "Optima" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",15000),
-                                    "Rio" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",8295),
-                                    "Rio" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",7500),
-                                    "Rio" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",11000)
-                                    ),
+$tab_voiture = array(
+    "Kia" => array(
+        "Forte" => array(
+            "description" => "",
+            "price" => array(
+                8995,
+                11000,
+                10995
+            )
+        ),
+        "Optima" => array(
+            "description" => "",
+            "price" => array(
+                10995,
+                12000,
+                15000
+            )
+        ),
+        "Rio" => array(
+            "description" => "",
+            "price" => array(
+                8295,
+                7500,
+                11000
+            )
+        )
+    ),
+    "Mazda" => array(
+        "CX3" => array(
+            "description" => "",
+            "price" => array(
+                23995,
+                15000,
+                12690
+            )
+        ),
+        "CX5" => array(
+            "description" => "",
+            "price" => array(
+                19995,
+                9000,
+                23000
+            )
+        ),
+        "CX7" => array(
+            "description" => "",
+            "price" => array(
+                7805,
+                9635,
+                6997
+            )
+        )
+    ),
+    "Toyota"=> array(
+        "corolla" => array(
+            "description" => "",
+            "price" => array(
+                15995,
+                13569,
+                12569
+            )
+        ),
+        "Tundra" => array(
+            "description" => "",
+            "price" => array(
+                19877,
+                22000,
+                18000
+            )
+        ),
+        "venza" => array(
+            "description" => "",
+            "price" => array(
+                16495,
+                6352,
+                7894
+            )
+        )
+    ),
+    "Mitsubichi"=> array(
+        "lancer" => array(
+            "description" => "",
+            "price" => array(
+                9595,
+                5600,
+                7500
+            )
+        ),
+        "mirage" => array(
+            "description" => "",
+            "price" => array(
+                15495,
+                20000,
+                13000
+            )
+        ),
+        "outlander" => array(
+            "description" => "",
+            "price" => array(
+                22995,
+                33000,
+                19999
+            )
+        )
+    ),
+    "Nissan"=> array(
+        "juke" => array(
+            "description" => "",
+            "price" => array(
+                9295,
+                15000,
+                13000
+            )
+        ),
+        "micra" => array(
+            "description" => "",
+            "price" => array(
+                12195,
+                19875,
+                15698
+            )
+        ),
+        "rogue" => array(
+            "description" => "",
+            "price" => array(
+                10977,
+                12000,
+                18000
+            )
+        )
+    )
+);
 
-                    "Mazda" => Array
-                                    (
-                                        "CX3" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",23995),
-                                        "CX3" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",15000),
-                                        "CX3" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",12690),
-                                        "CX5" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",19995),
-                                        "CX5" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",19995),
-                                        "CX5" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",9000),
-                                        "CX5" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",23000),
-                                        "CX7" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",7805),
-                                        "CX7" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",9635),
-                                        "CX7" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",6997)
-                                    ),
-                    "Toyota"=> Array
-                                    (
-                                        "corolla" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",15995),
-                                        "corolla" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",13569),
-                                        "corolla" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",12569),
-                                        "Tundra" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",19877),
-                                        "Tundra" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",22000),
-                                        "Tundra" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",18000),
-                                        "venza" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",16495),
-                                        "venza" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",6352),
-                                        "venza" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",7894)
-                                    ),
-                    "Mitsubichi"=> Array
-                                    (
-                                        "lancer" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",9595),
-                                        "lancer" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",5600),
-                                        "lancer" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",7500),
-                                        "mirage" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",15495),
-                                        "mirage" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",20000),
-                                        "mirage" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",13000),
-                                        "outlander" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",22995),
-                                        "outlander" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",33000),
-                                        "outlander" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",19999)
-                                    ),
-                    "Nissan"=> Array
-                                    (
-                                        "juke" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",9295),
-                                        "juke" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",15000),
-                                        "juke" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",13000),
-                                        "micra" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",12195),
-                                        "micra" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",19875),
-                                        "micra" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",15698),
-                                        "rogue" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",10977),
-                                        "rogue" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",12000),
-                                        "rogue" => Array("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.",18000)
-                                    )
-                    );
-
-
-
-/* foreach($tab_voiture as $cle1 => $valeur1)
-{
-  echo "clef1 : " . $cle1 . "<br/>";
-    foreach ($valeur1 as $cle2=>$valeur2)
-    {
-      echo "Clé2 : ".$cle2 .", Valeur: " . $valeur2 . "<br />\n";
-    }
-}   */
 ?>
-
