8ea19ca8ddb57b37405a8cee5b0f9f70f370c181
paullenoir <lenoirpaul76@gmail.com>
2019-03-12 13:19:10 -0400

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,39 +1,58 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/style.css">
-        <title>Accueil</title>
-    </head>
-    <body>
-        <header>
-            <img src="../images/logo.PNG" alt="logo" id="center" height="200"/> <!-- le faire dans une autre page -->
-        </header>
-        <div>
-                <form name="formulaire"  method="post" action="">
+<?php
+/**
+ * @author Paul LENOIR <1834889>
+ */
+$marque = (!empty($_POST['marque'])?$_POST['marque'] : "");
+if(($_POST['Modèle']) && !empty($_POST['Modèle'])){ 
+    $tab_modele_get = $_POST['Modèle']; 
+} 
+?>
+
+<div>
+<form name="formulaire"  method="post" action="">
+        <label for="Marque">Marque :
+            <span class = "custom_dropdown">
                 <select name="Marque">
                         <?php
-                            $tab_deserialiser_voiture = unserialize($_GET['voiture']); 
-                            foreach($tab_deserialiser_voiture as $cle1 => $valeur1){
-                                echo "<option value= '" . $cle1 . "' ".(($Marque == $cle1) ? "selected" : "").">";
-                                echo $cle1;
+                            $tab_marque = unserialize($_GET['tab_marque']); 
+
+                            for($i = 0; $i < count($tab_marque);$i++){
+                                if (isset($_POST['marque']) && $_POST['marque'] == $tab_marque[$i]){
+                                    $selected = 'selected=selected';
+                                }
+                                else{
+                                    $selected = '';
+                                };
+                                echo '<option value="'.$tab_marque[$i]. '"' . $selected . '>';
+                                echo $tab_marque[$i];
                                 echo "</option>";
-                            }
+                            } 
                         ?>
                 </select>
-                <br>
-                <select name="Modèle">
-                <?php
-                    foreach($tab_deserialiser_voiture as $_GET['marque'] => $valeur2){
-                        echo "<option value= $valeur2 >";
-                        echo $valeur2;
-                        echo "</option> <br>";
-                    }
-                ?>
-                </select>
-                <input type="submit" name="Rechercher" value="Rechercher"/>
-            </form>
-        </div>
-        <footer>
-        </footer>
-    </body>
-</html>
+            </span>
+        </label>
+        <br>
+        <select name="Modèle[]" multiple="multiple" size="4"> 
+            <?php
+                include_once("./models/voitures.php");
+                $tab_model = getModelsByMaker($_POST['marque']);
+
+                for($i = 0; $i < count($tab_model); $i++){
+                    echo "<option value= ".$tab_model[$i]." >";
+                    echo $tab_model[$i];
+                    echo "</option> <br>";
+                } 
+            ?>
+        </select>
+        <input type="submit" name="Rechercher" value="Rechercher"/>
+    </form>
+</div>
+<?php
+if($_POST['Rechercher'] != ""){
+    $tab_modele_selected = serialize($tab_modele_get);
+    header("location:./controllers/selection.php?tab_modele_selected=" .$tab_modele_selected);
+
+    //Benjamin pour recuperer les valeurs modeles selectionnees 
+    //$tab_modele_recu = unserialize($_GET['tab_modele_selected']); 
+}
+?>
