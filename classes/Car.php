a9b28bac17ab930e0816124b09fef13047db7791
3Nd_R1m <ben.bergeron1@gmail.com>
2019-03-14 21:49:47 -0400

    Edited Car class and databass

--- classes/Car.php
+++ classes/Car.php
@@ -1,25 +1,60 @@
 <?php
-define('CAR_IMAGE_FOLDER', '');
-
+define('THUMBNAIL_WIDTH', 680);
+define('THUMBNAIL_HEIGHT', 451);
 class Car {
     private static $Car_Class_Indexer = 0;
     public $maker;
     public $model_name;
     public $description;
     public $price;
+    public $thumbnail_path;
+    public $image_path;
     public $dbId;
 
-    public function __construct(string $maker, string $model_name, $price, $description)    
-    {    
+    public function __construct(string $maker, string $model_name, int $price, string $description) {
         $this->maker = $maker;
         $this->model_name = $model_name;
         $this->price = $price;
         $this->description = $description;
+        $this->thumbnail_path = './images/cars/thumbnails/'.$this.'.png';
+        $this->image_path = './images/cars/'.$this.'.png';
+        $this->generateThumbnail();
+
         $this->dbId = self::$Car_Class_Indexer++;
     }
 
-    public function __toString()
-    {
+    private static function imageCreateFromFile(string $filename) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $file_mime_type = finfo_file($finfo, $filename);
+        switch ($file_mime_type) {
+            case 'image/jpg':
+                return imagecreatefromjpeg($filename); break;
+            case 'image/png':
+                return imagecreatefrompng($filename); break;
+            case 'image/gif':
+                return imagecreatefromgif($filename); break;
+            case 'image/webp':
+                return imagecreatefromwebp($filename); break;
+            break;
+            // ...
+            default:
+                throw new InvalidArgumentException('File "'.$filename.'" is not a recognized image format.');
+            break;
+        }
+        finfo_close($finfo);
+    }
+
+    private function generateThumbnail() {
+        if(!file_exists($this->thumbnail_path)) {
+            $img_src = Car::imageCreateFromFile($this->image_path);
+            $img_dest = imagescale($img_src, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
+            imagepng($img_dest, $this->thumbnail_path);
+            imagedestroy($img_src);
+            imagedestroy($img_dest);
+        }
+    }
+
+    public function __toString() {
         return $this->maker . " " . $this->model_name;
     }
 }
--- models/voitures.php
+++ models/voitures.php
@@ -32,17 +32,17 @@ class Model {
                 #https://www.mazda.fr/showroom/
                 new Car("Mazda", "CX3", 20390, "Le Mazda CX-3 vous donne le sourire dès les premiers tours de roue. Ce crossover urbain a tout d’un grand séducteur : un design athlétique et élégant, un charme irrésistible et des prestations à la hauteur du niveau de performance. Grâce à l’évolution du design KODO, l’extérieur prend une allure audacieuse et raffinée et l’intérieur vous impressionne par le soin apporté à chaque détail et la qualité des finitions. "),
                 new Car("Mazda", "CX5", 24350, "Avec le Mazda CX-5, entrez dans une nouvelle dimension où le conducteur est placé au centre de toutes les attentions. Repensé de fond en comble, notre SUV compact est aussi agréable et confortable pour les passagers que pour le conducteur. Conçu pour célébrer le plaisir de conduire, le Mazda CX-5 suscite chez chaque occupant un puissant sentiment Jinba Ittai, cette formidable sensation d’osmose et de lien avec la voiture."),
-                new Car("Mazda", "Modèle 2", 16450, "La Mazda2, qui allie plaisir de conduire et style, est incomparable. Chaque Mazda est inspirée de la philosophie Jinba Ittai, pour créer une osmose entre le conducteur et la voiture, à l’instar de l’union entre le cavalier et sa monture. Ressentez le lien fusionnel qui vous unit à votre Mazda2 et qui se renforce chaque fois que vous parcourez les rues de la ville ou les routes sinueuses de la campagne. Les dernières Technologies SKYACTIV et SKYACTIV-VEHICLE DYNAMICS rendent ce lien encore plus intense et agréable."),
+                new Car("Mazda", "Cx7", 16450, "La Mazda2, qui allie plaisir de conduire et style, est incomparable. Chaque Mazda est inspirée de la philosophie Jinba Ittai, pour créer une osmose entre le conducteur et la voiture, à l’instar de l’union entre le cavalier et sa monture. Ressentez le lien fusionnel qui vous unit à votre Mazda2 et qui se renforce chaque fois que vous parcourez les rues de la ville ou les routes sinueuses de la campagne. Les dernières Technologies SKYACTIV et SKYACTIV-VEHICLE DYNAMICS rendent ce lien encore plus intense et agréable."),
                 #https://www.toyota.fr/
-                new Car("Toyota", "Corolla", 23950, "Berline compacte aux lignes racées et au regard affûté, la Nouvelle Corolla est prête à attaquer la route. Ressentez le dynamisme et l'énergie de ses deux motorisations Hybrides 122 ch et 180 ch, en profitant de sensations de conduite inégalées. Son look distinctif allié à un intérieur design et technologique font de la Nouvelle Corolla, la référence Hybride. Installez-vous pour vivre une expérience unique."),
-                new Car("Toyota", "Tundra", 31520, ""),
-                new Car("Toyota", "Venza", 16495, ""),
-                new Car("Mitsubichi", "Lancer", 9595, ""),
-                new Car("Mitsubichi", "Mirage", 20000, ""),
-                new Car("Mitsubichi", "Outlander", 22995, ""),
+                new Car("Toyota", "Corolla 2017", 23950, "Berline compacte aux lignes racées et au regard affûté, la Nouvelle Corolla est prête à attaquer la route. Ressentez le dynamisme et l'énergie de ses deux motorisations Hybrides 122 ch et 180 ch, en profitant de sensations de conduite inégalées. Son look distinctif allié à un intérieur design et technologique font de la Nouvelle Corolla, la référence Hybride. Installez-vous pour vivre une expérience unique."),
+                new Car("Toyota", "Highlander 2017", 31520, ""),
+                new Car("Toyota", "RAV4 2017", 16495, ""),
+                new Car("Mitsubichi", "Lancer 2016", 9595, ""),
+                new Car("Mitsubichi", "Mirage 2017", 20000, ""),
+                new Car("Mitsubichi", "Outlander 2013", 22995, ""),
                 new Car("Nissan", "Juke", 13000, ""),
-                new Car("Nissan", "Micra", 15698, ""),
-                new Car("Nissan", "Rogue", 10977, ""),
+                new Car("Nissan", "Sentra 2016", 15698, ""),
+                new Car("Nissan", "Versa", 10977, ""),
             );
         }
         if(true /*self::$makers_list == null*/)
