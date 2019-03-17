f94a158fa79f03a6a6edf5a0f08612b52104be00
3Nd_R1m <ben.bergeron1@gmail.com>
2019-03-17 12:17:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,22 +11,22 @@ $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'],'
 switch($url[0]){
     case 'financement': 
         include './controllers/financement.php';
+        generateView();
         break;
     case 'images':
         echo '<img src="' . $_SERVER['PATH_INFO'] . '"/>';
-        exit();
         break;
     case 'selection':
         include './controllers/selection.php';
+        generateView();
         break;
     case 'acceuil':
         include './controllers/accueil.php';
+        generateView();
         break;
     default:
         header('location: acceuil');
         break;
 }
-
-generateView();
 
 ?>
