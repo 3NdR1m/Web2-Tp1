ada7cd752cf6101051be9119d593704f2c0c5af4
paullenoir <lenoirpaul76@gmail.com>
2019-03-17 15:58:14 -0400

    Update accueil.php

--- controllers/accueil.php
+++ controllers/accueil.php
@@ -14,9 +14,9 @@ $maker = $car_makers[$selected_maker];
 $car_models = Model::getModelsByMaker($maker);
 
 $button_maker = (!empty($_POST['reload_models'])?$_POST['reload_models'] : " ");
+$button_model = (!empty($_POST['proceed'])?$_POST['proceed'] : " ");
 
-if(isset($_POST['proceed']) && isset($button_maker)) {
-   header("location:./controllers/selection.php?selected_cars='.$selected_cars"); 
-   
+if(isset($button_maker) && isset($button_model)) {
+  header("location:./controllers/selection.php?selected_cars='.$selected_cars"); 
 }
 ?>
