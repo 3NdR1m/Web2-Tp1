543167b9331892d2edede98ecaf536a5e595e6a5
paullenoir <lenoirpaul76@gmail.com>
2019-03-17 15:39:56 -0400

    paul

--- controllers/accueil.php
+++ controllers/accueil.php
@@ -13,36 +13,9 @@ $car_makers = Model::getMakers();
 $maker = $car_makers[$selected_maker];
 $car_models = Model::getModelsByMaker($maker);
 
-if(isset($_POST['proceed'])) {
-   echo 'yeet';
+$selected_models = (!empty($_POST['reload_models'])?$_POST['reload_models'] : " ");
+
+if(isset($_POST['proceed']) && isset($selected_models)) {
+   header("location:./controllers/selection.php");
 }
-
-
-/*
-if($_GET["acces"] != "true"){
-
-$tab_modele_1 = Model::getModelsByMaker($tab_marque[0]);    //tous les objets car de la 1ere marque
-
-//obtenir les modeles de la marque Kia
-   $tab_modele_2 = null;
-   $index = 0;
-   foreach($tab_modele_1 as $cle => $valeur1){ 
-         foreach($valeur1 as $cle2 => $valeur2){
-            if($cle2 == "model_name"){
-               $tab_modele_2[$index] = $valeur2;
-               $index++;
-            }
-         }
-   }
-
-//variable pour le header
-   $tab_marque_header = serialize(Model::getMakers());
-   $tab_modele_header = serialize($tab_modele_2);  
-}
-
-//traitement page accueil
-if($_GET["acces"] == "true"){
-   echo "toto";
-}
-   */
 ?>
